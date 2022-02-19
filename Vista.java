92219b6011126496eefa99a9bd75f5ae2af4f19d
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-02-18 19:23:18 -0600

    Agregando vista olvidada en primer commit

---
+++ Vista.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C), 2022-2023, FabianJuarez
+ * FileName: Controladora
+ * Author:   Fabián Juárez
+ * Date:     18/02/2022
+ * @author Fabian Juarez
+ */
+public class Vista{
+
+    public void bienvenida() {
+        System.out.println("------------- Bienvenido/bienvenida a su calculadora postfix ---------------- "); // Titulo
+    }
+    public void mensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+}
